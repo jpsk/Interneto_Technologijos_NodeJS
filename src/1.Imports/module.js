363144c59bbc4239b260c9a91985817028bbc952
Mindaugas Paskevicius <pask.mindaugas@gmail.com>
2020-12-14 00:05:52 +0200

    a

--- src/1.Imports/module.js
+++ src/1.Imports/module.js
@@ -1,1 +1,13 @@
 // export module
+
+let count = 0;
+function workForALongTime(time){
+    const start = new Date().getTime();
+    while(new Date().getTime() < start + time){}
+}
+function countSeconds(){
+    workForALongTime(2000);
+    count++;
+    console.log('count' + count);
+}
+setInterval(countSeconds, 1000);
